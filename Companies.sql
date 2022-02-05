53fe4654d824d168cf8b8049272301cf8bddeb8d
Svetlana <51971023+SvetlanaRedkina@users.noreply.github.com>
2022-02-05 17:12:10 -0500

    Update Companies.sql

--- Companies.sql
+++ Companies.sql
@@ -30,6 +30,7 @@ A note:
 Rank() without a partition will assign rank to each row based on RevenueBillions in descending order.
 Notice that there is a tie between 'Google Ads' and 'Google Other', and rank() assigns both rows to the same rank 9. 
 The next row's ('Physical Stores') rank value is incremented by 2 and is equal to 11. */
+
 SELECT RevenueStream,
 	 Company,
 	 RevenueBillions,
@@ -37,21 +38,24 @@ SELECT RevenueStream,
 		ORDER BY RevenueBillions DESC
 		) AS Ranked
 FROM Companies;
+
 /* Output:
-RevenueStream						 Company				RevenueBillions		Ranked
-iPhone								 Apple					166					1
-Online Stores        				 Amazon					123					2
-Advertising (Google Properties)		 Alphabet				96					3
-3rd Party Seller Services			 Amazon					42					4
-Services							 Apple					37					5
-Web Services						 Amazon					25					6
-iPad								 Apple					23					7
-Mac									 Apple					23					7
-Google Ads							 Alphabet				20					9
-Google Other					     Alphabet				20					9
-Physical Stores						 Amazon					17					11
-Other Products						 Apple					17					11
-Subscription Services				 Amazon					14					13 
+
+RevenueStream						 	Company					RevenueBillions		                Ranked
+
+iPhone							 	Apple					166					1
+Online Stores        				         	Amazon					123					2
+Advertising (Google Properties)				 	Alphabet				96					3
+3rd Party Seller Services				 	Amazon					42					4
+Services						 	Apple					37					5
+Web Services						 	Amazon					25					6
+iPad							 	Apple					23					7
+Mac							 	Apple					23					7
+Google Ads						 	Alphabet				20					9
+Google Other					         	Alphabet				20					9
+Physical Stores						 	Amazon					17					11
+Other Products						 	Apple					17					11
+Subscription Services				         	Amazon					14					13 
 */
 
 
@@ -59,6 +63,7 @@ Subscription Services				 Amazon					14					13
 A note: 
 The value of the rank is reset to 1 every time the query jumps on a new partition. 
 As in Task 1, 'Google Ads' and 'Google Other' are assigned to the same rank equal to 2. */
+
 SELECT RevenueStream,
 	 Company,
 	 RevenueBillions,
@@ -66,27 +71,32 @@ SELECT RevenueStream,
 		PARTITION BY Company ORDER BY RevenueBillions DESC
 		) AS RankedPartitioned
 FROM Companies;
+
 /* Output:
+
 RevenueStream							Company				RevenueBillions			RankedPartitioned
-Advertising (Google Properties)			Alphabet			96						1
-Google Ads								Alphabet			20						2
-Google Other							Alphabet			20						2
-Online Stores							Amazon				123						1
-3rd Party Seller Services				Amazon				42						2
-Web Services							Amazon				25						3
-Physical Stores							Amazon				17						4
-Subscription Services					Amazon				14						5
-iPhone									Apple				166						1
-Services								Apple				37						2
-iPad									Apple				23						3
-Mac										Apple				23						3
-Other Products							Apple				17						5
+
+Advertising (Google Properties)					Alphabet			96				1
+Google Ads							Alphabet			20				2
+Google Other							Alphabet			20				2
+Online Stores							Amazon				123				1
+3rd Party Seller Services					Amazon				42				2
+Web Services							Amazon				25				3
+Physical Stores							Amazon				17				4
+Subscription Services						Amazon				14				5
+iPhone								Apple				166				1
+Services							Apple				37				2
+iPad								Apple				23				3
+Mac								Apple				23				3
+Other Products							Apple				17				5
 */
+
 
 /* Task 3: Apply dense_rank() in order to rank the table based on Companies' Revenues in descending order.
 A note:  
 Dense_rank() works similarly to rank() but the ranks are assigned in a consecutive order.
 For instance, 'Google Ads' and 'Google Other' share the rank 8 but the next row, 'Physical Stores', gets the rank 9 (the value is incremented by 1 rather than 2). */
+
 SELECT RevenueStream,
 	 Company,
 	 RevenueBillions,
@@ -94,21 +104,24 @@ SELECT RevenueStream,
 		ORDER BY RevenueBillions DESC
 		) AS DenseRanked
 FROM Companies;
+
 /* Output:
-RevenueStream							Company		RevenueBillions		DenseRanked
-iPhone									Apple		166					1
-Online Stores							Amazon		123					2
-Advertising (Google Properties)			Alphabet	96					3
-3rd Party Seller Services				Amazon		42					4
-Services								Apple		37					5
-Web Services							Amazon		25					6
-iPad									Apple		23					7
-Mac										Apple		23					7
-Google Ads								Alphabet	20					8
-Google Other							Alphabet	20					8
-Physical Stores							Amazon		17					9
-Other Products							Apple		17					9
-Subscription Services					Amazon		14					10
+
+RevenueStream							Company				RevenueBillions			DenseRanked
+
+iPhone								Apple				166				1
+Online Stores							Amazon				123				2
+Advertising (Google Properties)					Alphabet			96				3
+3rd Party Seller Services					Amazon				42				4
+Services							Apple				37				5
+Web Services							Amazon				25				6
+iPad								Apple				23				7
+Mac								Apple				23				7
+Google Ads							Alphabet			20				8
+Google Other							Alphabet			20				8
+Physical Stores							Amazon				17				9
+Other Products							Apple				17				9
+Subscription Services						Amazon				14				10
 */
 
 
@@ -116,6 +129,7 @@ Subscription Services					Amazon		14					10
 A note:
 Since we use dense_rank(), when there is a tie between values within a partition, the next row's rank is incremented by 1.
 For example, 'iPad' and 'Mac' are assigned to rank 3. The next row, Apple's 'Other Products', is ranked 4. */
+
 SELECT RevenueStream,
 	 Company,
 	 RevenueBillions,
@@ -123,27 +137,31 @@ SELECT RevenueStream,
 		PARTITION BY Company ORDER BY RevenueBillions DESC
 		) AS DenseRankedPartitioned
 FROM Companies;
+
 /* Output:
-RevenueStream							Company		RevenueBillions		DenseRankedPartitioned						
-Advertising (Google Properties)			Alphabet	96					1
-Google Ads								Alphabet	20					2
-Google Other							Alphabet	20					2
-Online Stores							Amazon		123					1
-3rd Party Seller Services				Amazon		42					2
-Web Services							Amazon		25					3
-Physical Stores							Amazon		17					4
-Subscription Services					Amazon		14					5
-iPhone									Apple		166					1
-Services								Apple		37					2
-iPad									Apple		23					3
-Mac										Apple		23					3
-Other Products							Apple		17					4
+
+RevenueStream							Company				RevenueBillions			DenseRankedPartitioned
+
+Advertising (Google Properties)					Alphabet			96				1
+Google Ads							Alphabet			20				2
+Google Other							Alphabet			20				2
+Online Stores							Amazon				123				1
+3rd Party Seller Services					Amazon				42				2
+Web Services							Amazon				25				3
+Physical Stores							Amazon				17				4
+Subscription Services						Amazon				14				5
+iPhone								Apple				166				1
+Services							Apple				37				2
+iPad								Apple				23				3
+Mac								Apple				23				3
+Other Products							Apple				17				4
 */
 
 
 /* Task 5: Apply row_number() to rank the table based on RevenueBillions in descending order.
 A note:
 Row_number() generates a sequential number for each row based on RevenueBillions and does not skip any values. */
+
 SELECT RevenueStream,
 	 Company,
 	 RevenueBillions,
@@ -151,21 +169,24 @@ SELECT RevenueStream,
 		ORDER BY RevenueBillions DESC
 		) AS RowNumber
 FROM Companies;
+
 /* Output:
-RevenueStream							Company			RevenueBillions		RowNUmber
-iPhone									Apple			166					1
-Online Stores							Amazon			123					2
-Advertising (Google Properties)			Alphabet		96					3
-3rd Party Seller Services				Amazon			42					4
-Services								Apple			37					5
-Web Services							Amazon			25					6
-iPad									Apple			23					7
-Mac										Apple			23					8
-Google Ads								Alphabet		20					9
-Google Other							Alphabet		20					10
-Physical Stores							Amazon			17					11
-Other Products							Apple			17					12
-Subscription Services					Amazon			14					13
+
+RevenueStream							Company				RevenueBillions			RowNUmber
+
+iPhone								Apple				166				1
+Online Stores							Amazon				123				2
+Advertising (Google Properties)					Alphabet			96				3
+3rd Party Seller Services					Amazon				42				4
+Services							Apple				37				5
+Web Services							Amazon				25				6
+iPad								Apple				23				7
+Mac								Apple				23				8
+Google Ads							Alphabet			20				9
+Google Other							Alphabet			20				10
+Physical Stores							Amazon				17				11
+Other Products							Apple				17				12
+Subscription Services						Amazon				14				13
 */
 
 
@@ -174,6 +195,7 @@ A note:
 Row_number's logic is quite different from rank() and dense_rank() when we sort values withtin the partition.
 We can see that the row with Amazon's 'Subscription Services' and 14B in revenue is assigned to rank 1. 
 After that the rank's value is reset and the row with 'Physical Stores' and 17B in revenue is also assigned to rank 1.*/
+
 SELECT RevenueStream,
 	 Company,
 	 RevenueBillions,
@@ -181,27 +203,30 @@ SELECT RevenueStream,
 		PARTITION BY RevenueBillions ORDER BY RevenueBillions DESC
 		) AS RowNumberPartitioned
 FROM Companies;
+
 /* Output:
-RevenueStream					Company				RevenueBillions		RowNumberPartitioned
-Subscription Services			Amazon				14					1
-Physical Stores					Amazon				17					1
-Other Products					Apple				17					2
-Google Ads						Alphabet			20					1
-Google Other					Alphabet			20					2
-iPad							Apple				23					1
-Mac								Apple				23					2	
-Web Services					Amazon				25					1
-Services						Apple				37					1
-3rd Party Seller Services		Amazon				42					1
-Advertising (Google Properties) Alphabet			96					1
-Online Stores					Amazon				123					1
-iPhone							Apple				166					1
+
+RevenueStream							Company				RevenueBillions			RowNumberPartitioned
+Subscription Services						Amazon				14				1
+Physical Stores							Amazon				17				1
+Other Products							Apple				17				2
+Google Ads							Alphabet			20				1
+Google Other							Alphabet			20				2
+iPad								Apple				23				1
+Mac								Apple				23				2	
+Web Services							Amazon				25				1
+Services							Apple				37				1
+3rd Party Seller Services					Amazon				42				1
+Advertising (Google Properties) 				Alphabet			96				1
+Online Stores							Amazon				123				1
+iPhone								Apple				166				1
 */
 
 
 /* It is also helpful to know lead() and lag() functions, if one wants to get a preceding or succeeding value within a partition.
 Lead() function can be applied when we are dealing with the dates that overlap. The example below helps to illustrate it. 
 Let's create a table projectDates and insert some values into the table. */
+
 CREATE TABLE projectDates (
 	 Project_id INT NOT NULL AUTO_INCREMENT,
 	 Project_name VARCHAR(50) NOT NULL,
@@ -225,6 +250,7 @@ FROM projectDates;
 
 /* We can see that a student is working on both 'Advanced Joins' and 'Stored Procedures' projects from 2021-04-06 to 2021-04-08.
 Let's find out the number of days when both projects are in progress. */
+
 SELECT Project_name,
 	 In_project,
 	 Out_project,
@@ -232,8 +258,11 @@ SELECT Project_name,
 		ORDER BY In_project
 		) + 1 AS DaysInProjects
 FROM projectDates;
+
 /*Output:
-Project_name            In_project		Out_project		DaysInProjects
+
+Project_name            	In_project		Out_project		DaysInProjects
+
 Advanced Joins			2021-04-05		2021-04-08		3
 Stored Procedures		2021-04-06		2021-04-09		2
 ACID Transactions		2021-04-08		2021-04-12		-1
@@ -248,6 +277,7 @@ By the same logic, the query subtracts 2021-04-14 from 2021-04-12 and adds 1 (12
 
 /* The next problem explains lag() function and shows how it can be applied in a bit different setting.
 First, we create a table and insert values into it. */
+
 CREATE TABLE petCompanies (
 	Year INT NOT NULL,
 	Company VARCHAR(50) NOT NULL,
@@ -279,6 +309,7 @@ After that, lag function returns the preceeding value of Revenue.
 For instance, in 2019 'Cute Paws' has 49,000 in revenue in 'Pet Sitting' service and 31,000 in revenue in 'Pet Massage'.
 Lag() returns the preceeding value, 
 and if we look at the row with 'Pet Sitting', we will see that the other service the company provides, which is 'Pet Massage', yields smaller revenue. */
+
 SELECT Year,
 	 Company,
 	 Service,
@@ -287,20 +318,23 @@ SELECT Year,
 		PARTITION BY Year ORDER BY Company
 		) AS LagRevenue
 FROM petCompanies;
+
 /* Output:
-Year	Company			Service							Revenue		LagRevenue
-2019	Cute Paws		Pet Massage						31000		NULL
-2019	Cute paws		Pet Sitting						49000		31000
-2019	Pets First		Mobile Grooming					45000		49000
-2019	Pets First		Pets Consulting					14500		45000
-2019	Woof&Meow		Mobile Grooming					30000		14500
-2019	Woof&Meow		Food&Supplies Delivery			120000		30000
-2020	Cute Paws		Mobile Grooming					43500		NULL
-2020	Cute Paws		Pet Sitting						31000		43500
-2020	Pets First		Mobile Grooming					35000		31000
-2020	Pets First		Pet Consulting					25000		35000
-2020	Woof&Meow		Food&Supplies Delivery			61000		25000
-2020	Woof&Meow		Pet Photography					19000		61000
+
+Year		Company			Service					Revenue			LagRevenue
+
+2019		Cute Paws		Pet Massage				31000			NULL
+2019		Cute paws		Pet Sitting				49000			31000
+2019		Pets First		Mobile Grooming				45000			49000
+2019		Pets First		Pets Consulting				14500			45000
+2019		Woof&Meow		Mobile Grooming				30000			14500
+2019		Woof&Meow		Food&Supplies Delivery	 		120000			30000
+2020		Cute Paws		Mobile Grooming				43500			NULL
+2020		Cute Paws		Pet Sitting				31000			43500
+2020		Pets First		Mobile Grooming				35000			31000
+2020		Pets First		Pet Consulting				25000			35000
+2020		Woof&Meow		Food&Supplies Delivery			61000			25000
+2020		Woof&Meow		Pet Photography				19000			61000
 */
 
 
@@ -308,6 +342,7 @@ Year	Company			Service							Revenue		LagRevenue
 The only difference is that lead() is used to get value from row that succeeds the current row.
 Going back to 2019 'Cute Paws' example, we can see that the 'Pet Massage' returns 31,000 in revenue.
 The succeeding value is equal to 49,00 and comes from 'Pet Sitting' service. */
+
 SELECT Year,
 	 Company,
 	 Service,
@@ -316,18 +351,21 @@ SELECT Year,
 		PARTITION BY Year ORDER BY Company
 		) AS LeadRevenue
 FROM petCompanies;
+
 /* Output:
-Year	Company			Service							Revenue		LagRevenue
-2019	Cute Paws		Pet Massage						31000		49000
-2019	Cute paws		Pet Sitting						49000		45000
-2019	Pets First		Mobile Grooming					45000		14500
-2019	Pets First		Pets Consulting					14500		30000
-2019	Woof&Meow		Mobile Grooming					30000		120000
-2019	Woof&Meow		Food&Supplies Delivery			120000		NULL
-2020	Cute Paws		Mobile Grooming					43500		31000
-2020	Cute Paws		Pet Sitting						31000		35000
-2020	Pets First		Mobile Grooming					35000		25000
-2020	Pets First		Pet Consulting					25000		61000
-2020	Woof&Meow		Food&Supplies Delivery			61000		19000
-2020	Woof&Meow		Pet Photography					19000		NULL
+
+Year		Company			Service					Revenue			LagRevenue
+
+2019		Cute Paws		Pet Massage				31000			49000
+2019		Cute paws		Pet Sitting				49000			45000
+2019		Pets First		Mobile Grooming				45000			14500
+2019		Pets First		Pets Consulting				14500			30000
+2019		Woof&Meow		Mobile Grooming				30000			120000
+2019		Woof&Meow		Food&Supplies Delivery			120000			NULL
+2020		Cute Paws		Mobile Grooming				43500			31000
+2020		Cute Paws		Pet Sitting				31000			35000
+2020		Pets First		Mobile Grooming				35000			25000
+2020		Pets First		Pet Consulting				25000			61000
+2020		Woof&Meow		Food&Supplies Delivery			61000			19000
+2020		Woof&Meow		Pet Photography				19000			NULL
 */
